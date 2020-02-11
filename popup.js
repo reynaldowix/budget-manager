a89f18774cf6e8d9e396a329ebaf3ce1859fd090
Rei Puerto <59979491+reynaldowix@users.noreply.github.com>
2020-02-11 09:10:42 -0500

    From the budget manager
    https://www.youtube.com/watch?v=4HYDQAwowsc&list=PLC3y8-rFHvwg2-q6Kvw3Tl_4xhxtIaNlY&index=13

---
+++ popup.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('#spendAmount').click(function(){
+        chrome.storage.sync.get('total', function(budget){
+            var newTotal = 0;
+            if (budget.total){
+                newTotal += parseInt(budget.total);
+            }
+
+            var amount = $
+        })
+    })
+});
